bed38b70c3045b0cbaa2c485469c3244f5452559
SuperiorSam22 <sandeep221102@gmail.com>
2024-08-02 13:41:40 +0530

    added ticket model

---
+++ backend/models/Ticket.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+
+const ticketSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  severity: {
+    type: String,
+    enum: ["open", "closed", "on hold"],
+    default: "open",
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  comments: [
+    {
+      user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "user",
+      },
+      text: {
+        type: String,
+      },
+      date: {
+        type: Date,
+        default: Date.now,
+      },
+    },
+  ],
+});
+
+const Ticket = mongoose.model("Ticket", ticketSchema);
+
+module.exports = {
+    Ticket,
+}
