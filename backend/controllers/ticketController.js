6f8b0d25dd0ae18b503a379092f41101c2996ddc
SuperiorSam22 <sandeep221102@gmail.com>
2024-08-02 14:30:02 +0530

    added ticketController

---
+++ backend/controllers/ticketController.js
@@ -0,0 +1,125 @@
+const Ticket = require("../models/Ticket");
+const User = require("../models/User");
+
+//get all tickets
+//route GET /api/tickets
+const getTicket = async (req, res) => {
+  try {
+    const tickets = await Ticket.find().populate("user", "name email");
+    res.status(200).json(tickets);
+  } catch (err) {
+    res.status(500).json({ message: "Server Error" });
+  }
+};
+
+//Create a new ticket
+//route POST /api/tickets
+const createNewTicket = async (req, res) => {
+  const { title, description, severity, date } = req.body;
+  const userId = req.user._id;
+
+  try {
+    const newTicket = new Ticket({
+      title,
+      description,
+      severity,
+      date,
+      user: userId,
+    });
+    const ticket = await newTicket.save();
+    res.status(200).json(ticket);
+  } catch (error) {
+    res.status(400).json({ message: "Invalid Ticket Data" });
+  }
+};
+
+//Upadate a ticket
+//route PUT /api/tickets/:id
+const updateTicket = async (req, res) => {
+  const { severity, status, date } = req.body;
+
+  try {
+    const ticket = await Ticket.findbyId(req.params.id);
+
+    if (!ticket) {
+      return res.status(404).json({ message: "Ticket not found" });
+    }
+    ticket.severity = severity || ticket.severity;
+    ticket.status = status || ticket.status;
+    ticket.date = date || ticket.date;
+
+    const updatedTicket = await ticket.save();
+    res.json(updatedTicket);
+  } catch (error) {
+    res.status(400).json({ message: "Invalid Ticket data" });
+  }
+};
+
+//delete a ticket
+//route /api/tickets/:id\
+const deleteTicket = async (req, res) => {
+  try {
+    const ticket = await Ticket.findbyId(req.params.id);
+    if (!ticket) {
+      return res.status(404).json({ message: "Ticket not found" });
+    }
+
+    await ticket.remove();
+    res.status(200).json({ message: "Ticket removed" });
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+//Get ticket by ID
+//route /api/tickets/:id
+const getTicketById = async (req, res) => {
+  try {
+    const ticket = await Ticket.findbyId(req.params.id).populate(
+      "user",
+      "name email"
+    );
+
+    if (!ticket) {
+      return res.status(404).json({ message: "Ticket not found" });
+    }
+
+    res.status(200).json(ticket);
+  } catch (error) {
+    res.status(500).json({ message: "Server Error" });
+  }
+};
+
+//Add commnent to the ticket with id
+//route POST  /api/tickets/:id/comments 
+const addCommentToTicket = async (req, res) => {
+    try {
+        const ticket = await Ticket.findbyId(req.params.id);
+        if (!ticket) {
+            return res.status(404).json({ message: "Ticket not found" });
+        }
+
+        const comment = {
+            user: req.user._id,
+            text,
+        }
+
+        ticket.comment.push(comment);
+        await ticket.save();
+
+        res.status(200).json(ticket);
+
+    } catch (error) {
+        res.json(400).json({message: 'Invalid comment data'});
+    }
+}
+
+//export all
+module.exports = {
+    getTicket,
+    createNewTicket,
+    updateTicket,
+    deleteTicket,
+    getTicketById,
+    addCommentToTicket,
+}
