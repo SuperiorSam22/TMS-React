c1807fc3dcd409b545ab74c369a351e210404db3
SuperiorSam22 <sandeep221102@gmail.com>
2024-08-20 18:31:10 +0530

    multer2 added jwt

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,10 +2,15 @@ const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 const User = require("../models/User");
 
-
 //generate jwt
-const generateToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_SECRET, {
+const generateToken = (id, role, name, email) => {
+  const payload = {
+    id,
+    role,
+    name,
+    email,
+  };
+  return jwt.sign(payload, process.env.JWT_SECRET, {
     expiresIn: "30d",
     // algorithm: 'HS512', //use of different algorithm for encryption
   });
@@ -13,12 +18,7 @@ const generateToken = (id) => {
 
 //Register a new user
 const registerUser = async (req, res) => {
-  const { 
-    name,
-    email,
-    password,
-    role
-  } = req.body;
+  const { name, email, password, role } = req.body;
 
   try {
     //check for existing user email
@@ -40,7 +40,7 @@ const registerUser = async (req, res) => {
         name: user.name,
         email: user.email,
         role: user.role,
-        token: generateToken(user._id)
+        token: generateToken(user._id, user.role, user.name, user.email),
       });
       console.log("Created user: " + user);
     } else {
@@ -51,7 +51,6 @@ const registerUser = async (req, res) => {
     res.status(500).json({ message: "Internal server error" });
   }
 };
-
 
 //authenticate a user and get token
 //route POST /api/users/login
@@ -64,59 +63,55 @@ const loginUser = async (req, res) => {
     if (!user) {
       return res.status(404).json({ message: "User not found" });
     }
-    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
-            expiresIn: '10h',
-          });
     const passBcrypt = await bcrypt.compare(password, user.password);
     if (!passBcrypt) {
       return res.status(401).json({ message: "Invalid password" });
     }
 
-     return res.status(200).json({
+    return res.status(200).json({
       _id: user._id,
       name: user.name,
       email: user.email,
       role: user.role,
       token: generateToken(user._id, user.role, user.name, user.email),
     });
-      console.log(user);
-      console.log(token);
   } catch (error) {
-    console.log(error)
+    console.log(error);
     res.status(500).json({ message: "server error" });
   }
 };
 
 const userProfile = async (req, res) => {
-    try {
-        const user = await User.findById(req.user._id).select('-password');
+  try {
+    const user = await User.findById(req.user._id).select("-password");
 
-        if(user) {
-            res.status(200).json(user);
-        } else {
-            res.status(404).json({message: 'user not found'});
-        }
-
-    } catch (error) {
-      console.log(error);
-        res.status(500).json({message: "server error"});
+    if (user) {
+      res.status(200).json(user);
+    } else {
+      res.status(404).json({ message: "user not found" });
     }
-}
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "server error" });
+  }
+};
 
 const logoutUser = async (req, res) => {
   req.session.destroy((err) => {
     if (err) {
-      return res.status(500).json({ message: 'Logout failed, please try again' });
+      return res
+        .status(500)
+        .json({ message: "Logout failed, please try again" });
     }
 
-    res.clearCookie('connect.sid'); // Clears the session ID cookie
-    res.status(200).json({ message: 'Logout successful' });
+    res.clearCookie("connect.sid"); // Clears the session ID cookie
+    res.status(200).json({ message: "Logout successful" });
   });
-}
+};
 
 module.exports = {
-    registerUser,
-    loginUser,
-    userProfile,
-    logoutUser
-}
+  registerUser,
+  loginUser,
+  userProfile,
+  logoutUser,
+};
